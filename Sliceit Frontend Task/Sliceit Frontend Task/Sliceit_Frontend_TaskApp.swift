9345cd684768c0d5bcaacdf603d349c007f3362f
Saifullah Fazlani <saifullah.fazlani97@gmail.com>
2024-01-26 13:18:01 +0500

    Initial commit - Project creation

---
+++ Sliceit Frontend Task/Sliceit Frontend Task/Sliceit_Frontend_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sliceit_Frontend_TaskApp.swift
+//  Sliceit Frontend Task
+//
+//  Created by Saifullah Fazlani on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sliceit_Frontend_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
